ffe19b00ed8266d0c07a7c401463aa645778ae62
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-06-17 11:11:50 -0400

    modificado tabla

--- database/db.sql
+++ database/db.sql
@@ -1,17 +1,19 @@
-CREATE DATABASE database_nombre;
+CREATE DATABASE test;
 
-USE database_nombre;
+USE test;
 
-CREATE TABLE pacientes (
-    idpaciente int not null primary key auto_increment,
-    rut varchar(10),
-    nombre varchar(50),
-    apellidos varchar(50),
-    isdeleted bool
+CREATE TABLE persona (
+    
+    id_persona int(11) not null ,
+    nombre varchar(45) ,
+    apellido varchar(45) ,
+    email varchar(45) ,
+    telefono varchar(45) ,
+    activo tinyint(1)
 );
 
-alter table pacientes
-    add primary key (idpaciente);
+alter table persona
+    add primary key (id_persona);
 
 
 module.exports
