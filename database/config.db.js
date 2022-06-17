29eb7290391774cb56351534727aabfdc206de46
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-06-17 11:12:06 -0400

    cambio user

--- database/config.db.js
+++ database/config.db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'adminSakilla',
-  password: '@dmin.sakilla',
+  user: 'root',
+  password: '',
   database: 'test'
 });
 
