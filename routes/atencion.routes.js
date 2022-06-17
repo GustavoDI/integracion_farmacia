ef3ce1c3c5da560c60b79d37ed3d2404387c5a43
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2022-06-17 11:11:29 -0400

    add getbyid

--- routes/atencion.routes.js
+++ routes/atencion.routes.js
@@ -1,10 +1,17 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
-const {check, body}= require('express-validator');
+const { body } = require('express-validator');
 
 
-const {pacienteGet, pacientePut, pacienteoPost, pacienteDelete, pacienteGetAll, pacienteGetByID}= require ('../controllers/atencion.controller');
-const {validarCampos} = require('../middlewares/validar-campos')
+const { pacienteGet,
+    pacientePut,
+    pacienteoPost,
+    pacienteDelete,
+    pacienteGetAll,
+    pacienteDeleteEver,
+    pacienteGetByID } = require('../controllers/atencion.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
+
 // router.get('/',  (req, res)=> {
 //     // res.send('Buena cabros Tenemos casi ready el rest server de farmacia');
 //     res.json({
@@ -19,25 +26,27 @@ const {validarCampos} = require('../middlewares/validar-campos')
 // });
 
 router.get('/active-users', pacienteGet);
-router.get('/active-users-id/:id', pacienteGetByID);
+router.get('/user/:id', pacienteGetByID);
 router.get('/users', pacienteGetAll);
 
-router.post('/user',[
-    body('nombre', 'El nombre es obligatorio').not().isEmpty().isLength({min:3}),
-    body('apellido', 'El apellido es obligatorio').not().isEmpty().isLength({min:3}),
+router.post('/user', [
+    body('nombre', 'El nombre es obligatorio').not().isEmpty().isLength({ min: 3 }),
+    body('apellido', 'El apellido es obligatorio').not().isEmpty().isLength({ min: 3 }),
     body('email', 'El email no es valido').isEmail(),
     validarCampos
-],pacienteoPost);
+], pacienteoPost);
 
-router.put('/user/:id',[
-    body('nombre', 'El largo minimo es de tres').not().isEmpty().isLength({min:3}),
-    body('apellido', 'El largo minimo es de 3').not().isEmpty().isLength({min:3}),
+router.put('/user/:id', [
+    body('nombre', 'El largo minimo es de tres').not().isEmpty().isLength({ min: 3 }),
+    body('apellido', 'El largo minimo es de 3').not().isEmpty().isLength({ min: 3 }),
     body('email', 'El email debe ser valido').isEmail(),
     validarCampos
 ], pacientePut)
 
 /**Aqui deberia recibir el id del paciente /delete:id */
-router.delete('/delete/user/:id', pacienteDelete )
+router.delete('/delete/user/:id', pacienteDelete)
+router.delete('/delete-ever/user/:id', pacienteDeleteEver)
+
 
 router.get('*', (req, res) => {
     res.send('404 | Pagina no encontrada');
